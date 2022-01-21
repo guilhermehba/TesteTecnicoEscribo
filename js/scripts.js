a468362a9e60cf8edd307c004c41f3282f1e0361
Guilherme <guilhermemalka6@gmail.com>
2022-01-21 14:26:54 -0300

    alteração da h1 e remoção de um simbolo '=' do scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
-somar (10);
+calcular(10);
 
-function somar(limite) {
+function calcular(limite) {
     let multiplos3 = 0;
     let multiplos5 = 0;
-    for (i=0; i < limite; i++){
+    for (i = 0; i < limite; i++) {
         if (i % 3 === 0)
-        multiplos3 += i;
-        
-        if (i % 5 === 0)
-        multiplos5 += i;
-    }
-    console.log("A soma dos múltiplos de 3 e 5 abaixo do valor inserido é:",multiplos3+multiplos5);
-}
+            multiplos3 += i;
 
+        if (i % 5 === 0)
+            multiplos5 += i;
+    }
+    console.log("A soma dos múltiplos de 3 e 5 abaixo do valor inserido é:", multiplos3 + multiplos5);
+}
